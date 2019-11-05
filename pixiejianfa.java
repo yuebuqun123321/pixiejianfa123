2749cfa9ec12bde79c071d2a9fe70185d0f78b0a
linghuchong <428558292@qq.com>
2019-11-05 14:38:46 +0800

    confict

--- pixiejianfa.java
+++ pixiejianfa.java
@@ -2,5 +2,6 @@ public class pixiejianfa {
 	public static void main(String[] args) {
 		System.out.println("bi xian zi gong !");
 		System.out.println("no zuo no die");
+		System.out.println("jiu shi bu xing");
 	}
 }
